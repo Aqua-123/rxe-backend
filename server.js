2781124b1bcccd123348ac176159499c7f34d25e
Aqua <86239876+Aqua-123@users.noreply.github.com>
2024-06-07 21:47:21 +0530

    whoa

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import notesRoutes from "./routes/notes.js";
 import usersRoutes from "./routes/users.js";
 import bansRoutes from "./routes/bans.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -27,6 +28,14 @@ mongoose
 app.use("/notes", notesRoutes);
 app.use("/users", usersRoutes);
 app.use("/bans", bansRoutes);
+app.use(
+  cors({
+    origin: "*", // Allow all origins, you can specify your frontend URL here
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+    credentials: true, // Allow credentials if needed
+  })
+);
 // test route
 app.get("/", (req, res) => {
   res.send("Hello from Homepage");
